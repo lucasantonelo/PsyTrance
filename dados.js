22e2e6ef1a25bfdd067cf3f68a07bd54bf4848cb
lucasantonelo <lucas.antonelo@outlook.com>
2024-09-07 16:18:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Ajja",
+        descricao: "Ajja, DJ suíço, é conhecido por suas produções melódicas e atmosféricas, que mesclam elementos do psytrance com influências progressivas. Seus sets são caracterizados por uma jornada sonora envolvente, levando o público a um estado de transe profundo.",
+        link: "https://www.residentadvisor.net/dj/ajja",
+        tags: "ajja night"    
+    },
+    {
+        titulo: "Astrix",
+        descricao: "Astrix, nome artístico de Avi Shmailov, é um dos DJs e produtores de trance psicodélico mais renomados do mundo. Seu estilo único, que combina elementos do 'full-on' com batidas mais clubbier, o tornou um ícone da cena eletrônica.",
+        link: "https://pt.wikipedia.org/wiki/Astrix",
+        tags: "nariz brasil deep jungle walk"
+    },
+    {
+        titulo: "Goa Gil",
+        descricao: "Goa Gil foi um pioneiro do goa trance, tendo iniciado o movimento na Índia. Seu estilo único, que combinava elementos da música eletrônica com sonoridades indianas, influenciou gerações de DJs e produtores.",
+        link: "https://en.wikipedia.org/wiki/Goa_Gil",
+        tags: "lenda mestre"
+    }
+];
